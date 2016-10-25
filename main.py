131fe09c76e26351353776d3159019a1f461a62f
Vincent A <doezerx@gmail.com>
2016-10-25 13:50:27 +0200

    Started overhaul to add new knowledge of python

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ Created on 7/9/16
 @author: Vincent AIRIAU
 Version V0.1
 """
-
+import argparse
 import os
 import shutil
 import sys
@@ -15,7 +15,8 @@ import logging
 import requests
 from lxml import html
 
-logger = logging.basicConfig(format='%(asctime)s - %(module)s: %(message)s',filename='subscene_g.log', filemode='w', level=logging.DEBUG)
+logger = logging.basicConfig(format='%(asctime)s - %(module)s: %(message)s', filename='subscene_g.log', filemode='w',
+                             level=logging.DEBUG)
 
 
 def opensubscene_rls_page(showname):
@@ -38,63 +39,85 @@ class Subscene:
     def __init__(self, path):
         print "This is the path entered: " + path
         # Retrieve filename (for example TV.SHOW.XViD-EVO), so remove path and extension
-        showname = os.path.basename(path).replace('.avi', '').replace('.mkv', '').replace('.mpg', '')
+        episodename = os.path.basename(path).replace('.avi', '').replace('.mkv', '').replace('.mpg', '')
         localpath = os.path.dirname(path)
         zipfile_path = path.replace('.avi', '.zip').replace('.mkv', '.zip').replace('.mpg', '.zip')
 
-        # Retrieve corresponding subtitles list on subscene
-        sublist = getsubtitlelist(showname)
+
+    def downloadfirstsub(self):
+        sublist = self.getsubtitlelist(self.episodename)
         # If no subtitles, display a NOK message
         if not sublist:
             logger.ERROR("No subtitles are available for this release.")
         # If subtitles are present, download and unpack the first one
         else:
-            getsubtitlefile(sublist[0], zipfile_path)
-            print zipfile_path
-            with ZipFile(zipfile_path, 'r') as subtitle_zipfile:
+            self.getsubtitlefile(sublist[0], self.zipfile_path)
+            print self.zipfile_path
+            with ZipFile(self.zipfile_path, 'r') as subtitle_zipfile:
                 logger.INFO("extraction")
-                subtitle_zipfile.extractall(path=localpath)
-            os.rename(localpath + "\\" + subtitle_zipfile.namelist()[0], localpath + "\\" + showname + ".srt")
+                subtitle_zipfile.extractall(path=self.localpath)
+                os.remove(subtitle_zipfile)
+            os.rename(self.localpath + "\\" + subtitle_zipfile.namelist()[0], self.localpath + "\\" + self.episodename + ".srt")
+
+    def displaysubtitlelist(self):
+        sublist = self.getsubtitlelist(self.episodename)
 
 
-def getsubtitlelist(showname):
-    # Returns a table with subtitle URLs
-    # Or 0 if no subtitle
+    @staticmethod
+    def getsubtitlelist(episodename):
+        '''
 
-    tree = html.fromstring(opensubscene_rls_page(showname))
-    # sub_list_names = tree.xpath('//div[@id="content"]/div[2]/div/div/table/tbody/tr/td/a/span[2]/text()')
+        :param episodename: regex
+        :return: sub_list_links: a table with subtitle URLs or 0 if no subtitle
+        '''
+        # TODO: use regex to get the good or not take any
 
-    sub_list_links = tree.xpath('//div[@id="content"]/div[2]/div/div/table/tbody/tr/td/a/@href')
-    del sub_list_links[1::2]
-    return sub_list_links
+        tree = html.fromstring(opensubscene_rls_page(episodename))
+        # sub_list_names = tree.xpath('//div[@id="content"]/div[2]/div/div/table/tbody/tr/td/a/span[2]/text()')
+
+        sub_list_links = tree.xpath('//div[@id="content"]/div[2]/div/div/table/tbody/tr/td/a/@href')
+        del sub_list_links[1::2]
+        return sub_list_links
+
+    @staticmethod
+    def getsubtitlefile(subtitlerelativeURL, localpath):
+
+        base_url = 'https://subscene.com'
+        full_url = base_url + subtitlerelativeURL
+
+        user_agent = "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:47.0) Gecko/20100101 Firefox/47.0"
+        headers = {'User-Agent': user_agent}
+        req = urllib2.Request(full_url, '', headers)
+        response = urllib2.urlopen(req)
+        content = response.read()
+        tree = html.fromstring(content)
+        logger.INFO(content)
+        subtitle_link = tree.xpath('//a[@id="downloadButton"]/@href')[0]
+        subtitle_link = base_url + subtitle_link
+        logger.INFO(subtitle_link)
+        r = requests.get(subtitle_link, stream=True, headers={'User-Agent': user_agent})
+        if r.status_code == 200:
+            with open(localpath, 'wb') as f:
+                # r.raw.decode_content = True
+                shutil.copyfileobj(r.raw, f)
+                f.close()
+                logger.ERROR("archive enregistree")
+        else:
+            print "Subtitle couldn't be downloaded."
+            return 1
+        return 0
 
 
-def getsubtitlefile(subtitlerelativeURL, localpath):
+def parseArguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("file", help="The path to the video file. Takes only avi, mpg and mkv files.")
+    parser.add_argument("-l", action="store_true"", help="Use to list all the available subtitles and take the one you want.")
+    args = parser.parse_args()
+    return args
 
-    base_url = 'https://subscene.com'
-    full_url = base_url + subtitlerelativeURL
-
-    user_agent = "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:47.0) Gecko/20100101 Firefox/47.0"
-    headers = {'User-Agent': user_agent}
-    req = urllib2.Request(full_url, '', headers)
-    response = urllib2.urlopen(req)
-    content = response.read()
-    tree = html.fromstring(content)
-    logger.INFO(content)
-    subtitle_link = tree.xpath('//a[@id="downloadButton"]/@href')[0]
-    subtitle_link = base_url + subtitle_link
-    logger.INFO(subtitle_link)
-    r = requests.get(subtitle_link, stream=True, headers={'User-Agent': user_agent})
-    if r.status_code == 200:
-        with open(localpath, 'wb') as f:
-            # r.raw.decode_content = True
-            shutil.copyfileobj(r.raw, f)
-            f.close()
-            logger.ERROR("archive enregistree")
-    else:
-        print "Subtitle couldn't be downloaded."
-        return 1
-    return 0
-
-
-Subscene(str(sys.argv[1]))
+if __name__ == '__main__':
+    args = parseArguments()
+    if args.l:
+        Subscene(args.file).displaysubtitlelist()
+    else
+        Subscene(args.file).downloadfirstsub()
