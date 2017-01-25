d7939d187063091e89b5dc28268d4255ea196776
Vincent A <doezerx@gmail.com>
2017-01-25 19:37:30 +0100

    Started overhaul to add new knowledge of python

--- main.py
+++ main.py
@@ -4,19 +4,19 @@ Created on 7/9/16
 Version V0.1
 """
 import argparse
+import logging
 import os
+import re
 import shutil
-import sys
 import urllib
 import urllib2
 from zipfile import ZipFile
-import logging
 
 import requests
 from lxml import html
 
-logger = logging.basicConfig(format='%(asctime)s - %(module)s: %(message)s', filename='subscene_g.log', filemode='w',
-                             level=logging.DEBUG)
+logging.basicConfig(format='%(asctime)s - %(module)s: %(message)s', filename='subscene_g.log', filemode='w',
+                    level=logging.DEBUG)
 
 
 def opensubscene_rls_page(showname):
@@ -36,48 +36,69 @@ def opensubscene_rls_page(showname):
 
 
 class Subscene:
+
     def __init__(self, path):
         print "This is the path entered: " + path
         # Retrieve filename (for example TV.SHOW.XViD-EVO), so remove path and extension
-        episodename = os.path.basename(path).replace('.avi', '').replace('.mkv', '').replace('.mpg', '')
-        localpath = os.path.dirname(path)
-        zipfile_path = path.replace('.avi', '.zip').replace('.mkv', '.zip').replace('.mpg', '.zip')
-
+        self.episodename = os.path.splitext(os.path.basename(path))[0]
+        self.localpath = os.path.dirname(path)
+        self.zipfile_path = os.path.splitext(path)[0] + '.zip'
 
     def downloadfirstsub(self):
-        sublist = self.getsubtitlelist(self.episodename)
+        sublist = self.getsubtitle(self.episodename)
         # If no subtitles, display a NOK message
-        if not sublist:
-            logger.ERROR("No subtitles are available for this release.")
+        if not sublist or sublist == 0:
+            logging.error("No subtitles are available for this release.")
+            print "No subtitles are available for this release."
         # If subtitles are present, download and unpack the first one
         else:
-            self.getsubtitlefile(sublist[0], self.zipfile_path)
-            print self.zipfile_path
+            self.getsubtitlefile(sublist, self.zipfile_path)
+            # print self.zipfile_path
             with ZipFile(self.zipfile_path, 'r') as subtitle_zipfile:
-                logger.INFO("extraction")
+                logging.info("extraction")
                 subtitle_zipfile.extractall(path=self.localpath)
-                os.remove(subtitle_zipfile)
+                subtitle_zipfile.close()
+                os.remove(self.zipfile_path)
+            if os.path.exists(self.localpath + "\\" + self.episodename + ".srt"):
+                os.remove(self.localpath + "\\" + self.episodename + ".srt")
             os.rename(self.localpath + "\\" + subtitle_zipfile.namelist()[0], self.localpath + "\\" + self.episodename + ".srt")
 
     def displaysubtitlelist(self):
-        sublist = self.getsubtitlelist(self.episodename)
-
+        sublist = self.getsubtitle(self.episodename)
 
     @staticmethod
-    def getsubtitlelist(episodename):
+    def getsubtitle(episodename):
         '''
 
         :param episodename: regex
         :return: sub_list_links: a table with subtitle URLs or 0 if no subtitle
         '''
         # TODO: use regex to get the good or not take any
-
         tree = html.fromstring(opensubscene_rls_page(episodename))
+        first_word = episodename.split(".",1)[0]
         # sub_list_names = tree.xpath('//div[@id="content"]/div[2]/div/div/table/tbody/tr/td/a/span[2]/text()')
+        i = 1
+        sub_link = 0
 
-        sub_list_links = tree.xpath('//div[@id="content"]/div[2]/div/div/table/tbody/tr/td/a/@href')
-        del sub_list_links[1::2]
-        return sub_list_links
+        while 1:
+            sub_name = str(tree.xpath('//div[@class="content"]/table/tbody/tr[' + str(i) + ']/td[@class="a1"]/a/span[2]/text()[1]')[0]).strip()
+            # print "sub_name : " + sub_name
+            if sub_name == episodename:
+                sub_link = tree.xpath('//div[@class="content"]/table/tbody/tr[' + str(i) + ']/td[@class="a1"]/a/@href')[0]
+                # print "sub_list_links : " + sub_link
+                break
+            elif re.compile(first_word).search(sub_name):
+                sub_link = tree.xpath('//div[@class="content"]/table/tbody/tr[' + str(i) + ']/td[@class="a1"]/a/@href')[0]
+                # print "sub_list_links : " + sub_link
+                break
+            elif not sub_name:
+                break
+            else:
+                i += 1
+                if not tree.xpath('//div[@class="content"]/table/tbody/tr[' + str(i) + ']/td[@class="a1"]/a/span[2]/text()[1]'):
+                    break
+
+        return sub_link
 
     @staticmethod
     def getsubtitlefile(subtitlerelativeURL, localpath):
@@ -91,17 +112,17 @@ class Subscene:
         response = urllib2.urlopen(req)
         content = response.read()
         tree = html.fromstring(content)
-        logger.INFO(content)
+        logging.info(content)
         subtitle_link = tree.xpath('//a[@id="downloadButton"]/@href')[0]
         subtitle_link = base_url + subtitle_link
-        logger.INFO(subtitle_link)
+        logging.info(subtitle_link)
         r = requests.get(subtitle_link, stream=True, headers={'User-Agent': user_agent})
         if r.status_code == 200:
             with open(localpath, 'wb') as f:
                 # r.raw.decode_content = True
                 shutil.copyfileobj(r.raw, f)
                 f.close()
-                logger.ERROR("archive enregistree")
+                logging.info("archive enregistree")
         else:
             print "Subtitle couldn't be downloaded."
             return 1
@@ -120,4 +141,5 @@ if __name__ == '__main__':
     if args.l:
         Subscene(args.file).displaysubtitlelist()
     else:
-        Subscene(args.file).downloadfirstsub()
+        grabber = Subscene(args.file)
+        grabber.downloadfirstsub()
